d056f277fb9aeb3cb8dbe298e4f9634824a92a87
Jack Tyler <tyler.jackoliver@gmail.com>
2016-10-30 13:04:04 +0000

    Added IR+Servo test function

---
+++ ir+servotest/ir_servotest_ino/ir_servotest_ino.ino
@@ -0,0 +1,27 @@
+/* Function to actuate servo based on IR sensor output data */
+
+#include <Servo.h> // Include servo operation library
+
+int sensorPin = 0; //analog pin 0
+int pos = 0; // variable to store sensor position
+Servo myservo; // create servo object
+
+void setup(){
+	myservo.attach(9); // attaches servo on pin 9 to servo library
+	Serial.begin(9600); // Begin comms via Serial at 9600Hz
+}
+
+void loop(){
+	if (analogRead(sensorPin) < 100){ //  If reflectivity < 100
+		for (pos = 0; pos <= 180; pos += 5){ //  from 0 to 180 degrees
+			myservo.write(pos); // go to position
+//			delay(15);
+		}
+	}
+	if (analogRead(sensorPin) > 100){ // If reflectivity > 100
+		for (pos = 180; pos >= 0; pos -= 5){ // As above
+			myservo.write(pos);
+//			delay(15);
+		}		 
+	}
+}
