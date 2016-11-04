549ff60fa7616974c1067823b87fa1adb47cac91
Jack Tyler <tyler.jackoliver@gmail.com>
2016-11-04 13:02:33 +0000

    Help.

--- main_function_ino/main_function_ino.ino
+++ main_function_ino/main_function_ino.ino
@@ -2,11 +2,11 @@
 #include <Servo.h>                                            // Calls for Servo library
 
 #define MD25ADDRESS         0x58                              // Address of the MD25
-#define SPEED1              0x00                              // Byte to send speed to both motors for forward and backwards motion if operated in MODE 2 or 3 and Motor 1 Speed if in MODE 0 or 1
+#define SPEED1              (byte) 0x00                              // Byte to send speed to both motors for forward and backwards motion if operated in MODE 2 or 3 and Motor 1 Speed if in MODE 0 or 1
 #define SPEED2              0x01                              // Byte to send speed for turn speed if operated in MODE 2 or 3 and Motor 2 Speed if in MODE 0 or 1
 #define ENCODERONE          0x02                              // Byte to read motor encoder 1
 #define ENCODERTWO          0x06                              // Byte to read motor encoder 2
-#define ACCELERATION        0xE                               // Byte to define motor acceleration
+#define ACCELERATION        0xE                               // Byte to def===ine motor acceleration
 #define CMD                 0x10                              // Byte to reset encoder values
 #define MODE_SELECTOR       0xF                               // Byte to change between control MODES
 
@@ -277,7 +277,7 @@ void dispenserBack(){                                         // Move the servo 
                 dropServo.write(pos);                          // Move that servo, gurl. Werk it.  
         }
 
-
+}
 
 
 
@@ -285,7 +285,7 @@ void dispenserBack(){                                         // Move the servo 
 
 
 void loop(){
-
+  drop();
   mod_forward(100, CF);                                        // Traverse the first part of the course
   mod_rotate(90, CF);                                          // Rotate the first corner 
   drop();
